24049e3edfc3d0645b0997dcebdc25a0a73d3ce0
SFang-cmd <57546442+SFang-cmd@users.noreply.github.com>
2022-01-28 20:37:21 -0500

    Robot Vision Testing Stuff

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,84 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+
+import frc.robot.commands.*;
+import frc.robot.commands.vision.*;
+import frc.robot.subsystems.Subsystems;
+import frc.robot.userinterface.UserInterface;
+
+/**
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Robot extends TimedRobot {
+  
+	public Robot(){
+    	super(0.08);
+	}
+
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
+  	@Override
+	public void robotInit() {    
+		Subsystems.driveBase.setDefaultCommand(new TankDrive());
+
+		// Might need to be moved to teleopPeriodic in order to function, but it might also work here.
+		UserInterface.driverController.A.whenPressed(new RotateToBall());
+		UserInterface.driverController.X.whenPressed(new FollowBall());
+  	}
+
+	@Override
+  	public void robotPeriodic() {
+    	CommandScheduler.getInstance().run();
+  	}
+
+	@Override
+	public void autonomousInit() {
+		System.out.println("Autonomous Initalized");
+    	CommandScheduler.getInstance().cancelAll();
+	}
+
+	@Override
+	public void autonomousPeriodic() {
+
+	}
+
+	@Override
+	public void teleopInit() {
+		System.out.println("TeleOp Initalized");
+		CommandScheduler.getInstance().cancelAll();
+	}
+
+  @Override
+	public void teleopPeriodic() {
+		// Might be janky
+		if(UserInterface.driverController.getAButton()){
+			Subsystems.frontCam.snapshot(true);
+		}
+	}
+
+  @Override
+	public void disabledInit() {
+		System.out.println("Disabled Initialized");
+	    CommandScheduler.getInstance().cancelAll();
+	}
+
+	@Override
+	public void disabledPeriodic() {}
+
+	@Override
+	public void testInit() {}
+
+	@Override
+	public void testPeriodic() {}
+}
---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,83 @@
+package frc.robot;
+
+public class RobotMap {
+    // Robot Map is a centralized location at which all the numbers (ports, set speeds, etc) for the Robot can be placed
+    // so that it can be replaced easily later if need be. 
+    
+    // Speed controls
+    public static double speedCap = 0.8;
+    
+    // Robot Dimensions
+    public static double wheelDiameter = 7;
+
+    // motor port numbers
+    public enum RobotType{
+        COMP, PBOT, TOASTER 
+    }
+
+    public static int FlykS = 0;
+    public static int FlykV = 0;
+    public static int FlykA = 0;
+
+    
+    
+    // Left side motor port numbers
+    public static int leftMiddleMasterPort = 2;
+
+    public static int leftFrontFollower = 3;
+    public static int leftBackFollower = 6;
+    
+    // Right side motor port numbers
+    public static int rightMiddleMasterPort = 14;
+    
+    public static int rightFrontFollower = 5;
+    public static int rightBackFollower = 4;
+
+    // Falcon drive ports
+    public static int leftFront = 4;
+    public static int rightFront = 3;
+    public static int leftBack = 2;
+    public static int rightBack = 1;
+
+    
+    // Intake stuff
+    // public static int intakePivot = 46;
+    // public static int intakeLeftArm = 9;
+    // public static int intakeRightArm = 10;
+    // public static int lift = 11;
+    public static int extensionMotorPort = 422;
+    public static int intakeMotorPort = 422;
+
+    // Shooter
+    public static int rightFlyPort = 422;
+    public static int leftFlyPort = 422;
+
+    // Dimensions for Vision Calculations
+    public static double cameraHeightMeters = 0.25;
+    public static double ballHeightMeters = 0.1;
+    public static double cameraDegreesHoriz = -25;
+
+    /**
+     * @param inches Inches to convert.
+     * @return The equivalent distance in ticks (units for Talon Encoders).
+     */
+    public static double convertToTicks(double inches) {
+        return (4096 / (wheelDiameter * 3.1415926) * inches); // Math.PI
+    }
+
+    /**
+     * 
+     * @param value the arbitrary amount inputted
+     * @param min value can't be smaller than min
+     * @param max value can't be larger tahn max
+     * @return returns the cap if the value is too large/small, but will default to value if it does not reach the cap
+     */
+    public static double cap(double value, double min, double max){
+        if(value < min){
+            return min;
+        } else if (value > max){
+            return max;
+        }
+        return value;
+    }
+}
